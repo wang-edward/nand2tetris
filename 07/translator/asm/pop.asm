3a49940a8fee6a445873de5fb4e66cee8f527c2e
Edward Wang <edawordwang@gmail.com>
2024-01-13 21:49:06 -0500

    cleanup push and pop

--- 07/translator/asm/pop.asm
+++ 07/translator/asm/pop.asm
@@ -1,30 +1,8 @@
-// setup
-// -----------------------------
-
-// to begin, set LOCAL to 64
-@64
-D = A
-@LCL
-M = D
-// set SP to 256
-@256
-D = A
-@SP
-M = D
-// set the value at SP to 29
-@29
-D = A
-@255
-M = D
-
-// pop
-// -----------------------------
-
-// SP--
-@SP
-M = M - 1 // x = ram[SP]
-
-A = M
-D = M
-@{segment}.{index}
-M = D
+// pop {
+    @SP
+    M = M - 1
+    A = M
+    D = M
+    @{segment}.{index}
+    M = D
+// }
